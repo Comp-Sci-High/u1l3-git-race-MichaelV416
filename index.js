4461c8b8792e2d915df7a0d3830d531ccb5c01d7
MichaelV416 <michael.valles26@compscihigh.org>
2024-09-18 15:20:46 +0000

    Task 1 and 2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Task 1
 // Print your name
-
+"Michael Valles"
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+"Skittles"
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+"Straykids"
 // Stage and commit this change with an appropriate message
 // Go to more.js
