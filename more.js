be277d2a7a1c09bf036573748fc9968de75c47c5
MichaelV416 <michael.valles26@compscihigh.org>
2024-09-18 15:20:32 +0000

    Task 4 and 5

--- more.js
+++ more.js
@@ -1,9 +1,9 @@
 // Task 4
 // Print your favorite food space
-
+"Pizzaeria"
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+"Plain bagel with creamcheese"
 // Stage and commit this change with an appropriate message
 // Go to README.md
